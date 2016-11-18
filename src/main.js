fceccac93b251a3a20fb8f925b5e3886bc4dcdca
Jacob Payne <jacobapayne@gmail.com>
2016-11-19 01:33:52 +0300

    Setup Simple Vuex Store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 
-new Vue({
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    promises: new Set()
+  },
+  mutations : {
+    createPromise: (state, promise) => state.promises.add(promise),
+    deletePromise: (state, promise) => state.promises.delete(promise)
+  }
+})
+
+const app = new Vue({
   el: '#app',
   render: h => h(App)
 })
