56ae2e3ba5c029832d789065546a4176ec8a71f9
kondr1 <kondr179@rambler.ru>
2016-11-19 17:34:58 +0300

    Add input material style

--- src/routes/Create.vue
+++ src/routes/Create.vue
@@ -1,9 +1,30 @@
 <template lang="html">
   <form class="item_adder">
-    <input type="text" name="name" v-model="title" placeholder="Title">
-    <input type="text" name="name" v-model="desc" placeholder="Description">
-    <input type="text" name="name" v-model="assigne" placeholder="Assigne">
-    <input type="time" name="name" v-model="expire" placeholder="Expiration">
+
+    <div class="group">
+    	<input type="text" name="name" v-model="title" placeholder="Title">
+    	<span class="highlight"></span>
+      	<span class="bar"></span>
+      	<label>Title</label>
+    </div>
+    <div class="group">
+    	<input type="text" name="name" v-model="desc" placeholder="Description">
+    	<span class="highlight"></span>
+    	<span class="bar"></span>
+    	<label>Description</label>
+    </div>
+    <div class="group">
+    	<input type="text" name="name" v-model="assigne" placeholder="Assigne">
+    	<span class="highlight"></span>
+      	<span class="bar"></span>
+      	<label>Assigne</label>
+    </div>
+    <div class="group">
+    	<input type="time" name="name" v-model="expire" placeholder="Expiration">
+    	<span class="highlight"></span>
+      	<span class="bar"></span>
+      	<label>Expiration</label>
+    </div>
     <input type="button" value="Save">
   </form>
 </template>
@@ -14,5 +35,6 @@ export default {
 </script>
 
 <style lang="css">
+
 
 </style>
