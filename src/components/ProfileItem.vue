57f8de7e7e0c537f3ede5ece3489c7707cc3d44c
Jacob Payne <jacobapayne@gmail.com>
2016-11-19 15:41:57 +0300

    Seperate PromiseItem in Component

---
+++ src/components/ProfileItem.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <li class="profile-item">
+    <span class="time-limit"></span>
+    <header>
+      <h3> {{ item.title }}</h3>
+      <p>{{ item.description }}</p>
+    </header>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
+
+<style lang="css">
+
+  li {
+    display: flex;
+    flex-direction: row;
+    text-align: left;
+    padding: 1rem;
+  }
+
+  li span {
+    height: 4rem;
+    width: 4rem;
+  }
+
+  li header {
+    display: flex;
+    flex-direction: column;
+  }
+
+  li h3 {
+    margin: 0;
+  }
+
+  li:hover {
+    background-color: #888;
+    color: #fff;
+  }
+  li:active {
+    background-color: #444;
+  }
+</style>
--- src/routes/List.vue
+++ src/routes/List.vue
@@ -1,12 +1,6 @@
 <template lang="html">
   <ul>
-    <li v-for="item in list">
-      <header>
-        <h3> Обещание {{ item.title }}</h3>
-        <p>{{ item.description }}</p>
-      </header>
-      <input type="checkbox" name="name" class="checked" v-model="item.done">
-    </li>
+    <profile-item v-for="item in list" v-bind:item="item"></profile-item>
     <form class="item_adder">
       <input type="text" name="name" v-model="new_title" placeholder="Title">
       <input type="text" name="name" v-model="new_desc" placeholder="Description">
@@ -16,7 +10,13 @@
 </template>
 
 <script>
+
+import ProfileItem from '../components/ProfileItem.vue'
+
 export default {
+  components: {
+    ProfileItem
+  },
   computed: {
     list() { return this.$store.state.list }
   },
@@ -36,33 +36,6 @@ ul {
   padding: 0;
   margin: 0;
 }
-
-li {
-  display: flex;
-  flex-direction: row;
-  text-align: left;
-  justify-content: space-between;
-  padding: 1rem;
-}
-
-li header {
-  display: flex;
-  flex-direction: column;
-}
-
-li h3 {
-  margin: 0;
-}
-
-li:hover {
-  background-color: #888;
-  color: #fff;
-}
-li:active {
-  background-color: #444;
-}
-
-
 
 a {
   color: #42b983;
